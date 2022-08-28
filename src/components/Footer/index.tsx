a8079f651f72672556b729f47cb6180c4c40c232
alexsandro-cristiano <acg.alexsandro@hotmail.com>
2022-08-28 11:01:46 -0300

    refactory(Footer):modifica componente footer; modifica css do component navbar; adiciona biblioteca de icone

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,8 +1,30 @@
-import style from './Footer.module.scss'
+import { FiGithub, FiLinkedin } from 'react-icons/fi'
+import styles from './Footer.module.scss'
+
 export function Footer() {
   return (
-    <footer className={style.footer}>
-      <p> &copy; 2022 | Desenvolvido por Alexsandro Silva.</p>
+    <footer className={styles.footer}>
+      <section className={styles.direitos}>
+        <p>&copy; 2022 | Desenvolvido por Alexsandro Silva.</p>
+      </section>
+      <section className={styles.midiaSocial}>
+        <h3>Conecte-se</h3>
+        <ul>
+          <li>
+            <a
+              href="https://www.linkedin.com/in/alexsandro-cristiano"
+              target="_blank"
+            >
+              <FiLinkedin size={24} />
+            </a>
+          </li>
+          <li>
+            <a href="https://github.com/alexsandro-cristiano" target="_blank">
+              <FiGithub size={24} />
+            </a>
+          </li>
+        </ul>
+      </section>
     </footer>
   )
 }
