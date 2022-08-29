17f15b1534b03a1423f5fc88f6ebd7681185211d
alexsandro-cristiano <acg.alexsandro@hotmail.com>
2022-08-29 10:56:49 -0300

    feet(modulo vazio):cria componente emptymodule e adiciona logica basica de render em myleague

--- src/pages/Myleague/index.tsx
+++ src/pages/Myleague/index.tsx
@@ -1,9 +1,21 @@
+import coringaImg from 'assets/images/coringa.png'
+import { Emptymodule } from 'components/Emptymodule'
+import styles from './Myleague.module.scss'
+
 export function MyLeague() {
+  const liga = null
   return (
     <main>
-      <div>
-        <p> para card que terei</p>
-      </div>
+      {liga == '' ? (
+        <div>
+          <p>true</p>
+        </div>
+      ) : (
+        <Emptymodule
+          title="Sua liga ainda não tem nenhum membro!"
+          subtitle="Convide algum dos heróis para ela."
+        />
+      )}
     </main>
   )
 }
