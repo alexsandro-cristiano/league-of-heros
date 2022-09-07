2b27e0b53407f3f9145c86f38fea381d1f9e2138
alexsandro-cristiano <acg.alexsandro@hotmail.com>
2022-09-07 17:23:16 -0300

    refactoring:modifica a estrutura de AppRoutes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
-import React from 'react'
-import { AppRoutes } from 'routes/AppRoutes'
+import { Footer } from 'components/Footer';
+import { Navbar } from 'components/Navbar';
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { AppRoutes } from 'routes/AppRoutes';
 
-function App() {
-  return <AppRoutes />
+export function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <AppRoutes />
+        <Footer />
+      </BrowserRouter>
+    </>
+  );
 }
-
-export default App
--- src/routes/AppRoutes.tsx
+++ src/routes/AppRoutes.tsx
@@ -1,24 +1,18 @@
-import { Footer } from 'components/Footer'
-import { Navbar } from 'components/Navbar'
-import { Figurinha } from 'pages/Figurinha'
-import { Home } from 'pages/Home'
-import { MyLeague } from 'pages/Myleague'
-import { Notfound } from 'pages/Notfound'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import { Figurinha } from 'pages/Figurinha';
+import { Home } from 'pages/Home';
+import { MyLeague } from 'pages/Myleague';
+import { Notfound } from 'pages/Notfound';
+import { Route, Routes } from 'react-router-dom';
 
 export function AppRoutes() {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/">
-          <Route index element={<Home />} />
-          <Route path="myleague" element={<MyLeague />} />
-        </Route>
-        <Route path="figurinha/:id" element={<Figurinha />} />
-        <Route path="*" element={<Notfound />} />
-      </Routes>
-      <Footer />
-    </Router>
-  )
+    <Routes>
+      <Route path="/">
+        <Route index element={<Home />} />
+        <Route path="myleague" element={<MyLeague />} />
+      </Route>
+      <Route path="figurinha/:id" element={<Figurinha />} />
+      <Route path="*" element={<Notfound />} />
+    </Routes>
+  );
 }
